5f57d1f5058820dccaad930acdbc284984f27f37
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-03-28 12:14:46 +0000

    Adding Project Files, setting up header

---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+  return (
+    <div className="h-5/6 w-full flex-center flex-col">
+      <p className="hero-title">iPhone 15 Pro</p>
+    </div>
+  );
+};
+export default Hero;
